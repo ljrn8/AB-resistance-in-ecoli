0e84b979cbbcd4dfd3d1d5391b17f4fccc5ef475
Joel Hoefs <joel.hoefs7@gmail.com>
2023-07-24 04:27:07 +0800

    oh no

--- scripts/preprocess_variants.py
+++ scripts/preprocess_variants.py
@@ -2,36 +2,93 @@ import numpy as np
 import pandas as pd
 import os
 import pickle
+import heapq
+import pickle
 
-# try one with snps and compare with snps + indels ? 
+def get_loci(only_snps=True):
+    
+    # hashable check ? 
+    unique_set = set()
+    loci = []
 
-# try unsigned np.int32
-# snp_matrix = np.array(range(0, 1509), dtype=np.int64)
-# print(snp_matrix)
+    # do 1 pass first 
+    with open("../dataset/0_snps.txt", "r") as f:
+        loci = list(np.loadtxt(f, dtype=int, usecols=(0,)))
+        print('should look like this: ', np.array(loci))
+        
+    for count, file in enumerate(os.scandir("../dataset")):
+        genome_id, variant_type = file.name.rstrip(".txt").split("_")
+        
+        if only_snps and variant_type != 'snps':
+            continue
+        
+        if count % 20 == 0:
+            # only reflects snp count
+            print(f'\n{count/2}/1508: ', loci[:10], loci[-10:-1], len(loci))
+        
+        with open("../dataset/" + file.name, "r") as f:
+        
+            burst = list(np.loadtxt(f, dtype=int, usecols=(0,)))
 
-# df = pd.DataFrame(columns=['genome_id', 'snps']) 
-# df['genome_id'] = pd.Series(range(0, 1509))
-# print(df)
+            # extract only unqiue loci
+            common_set = unique_set.intersection(burst)
+            unique_snps = list(set(burst) - common_set)                 
+            
+            print(len(unique_snps), ", ", end="")
+                
+            for snp in unique_snps:
+                unique_set.add(snp)
+                
+            loci.extend(unique_snps)
 
-class loci_stack(list):
-    def insertion_sort(self, item) -> None:
-        self.append(item)
-        for step in range(1, len(self)):
-            key = self[step]
-            j = step - 1
-            while j >= 0 and key < self[j]:
-                self[j + 1] = self[j]
-                j = j - 1
-            self[j + 1] = key 
+            # Convert the extended array into a min-heap
+            heapq.heapify(loci)
+            
+    with open("../results/positions", "wb") as loci_file:
+        pickle.dump(loci, loci_file)
+        
 
 
+def sort_heap(heap_file="../results/loci"):
+    with open(heap_file, "rb") as loci_file:
+        loci_heap = pickle.load(loci_file)
+        
+    loci = [heapq.heappop(loci_heap) for i in range(len(loci_heap))]
+    with open("../results/positions_sorted", "wb") as loci_file:
+        pickle.dump(loci, loci_file)
+    
+
+def view_matrix(snp_matrix):
+    i = 0
+    for key in snp_matrix:
+        variants = snp_matrix[key]
+        print(f'{key}: {len(variants)}, {variants[:20]}')
+        
+        if i == 10:
+            return
+        i += 1
 
 
-
-
-def add_snps() -> np.array:
+def generate_snp_matrix():
+    with open("../results/positions_sorted", "rb") as loci_file:
+        loci = pickle.load(loci_file)
+    
+    print(np.array(loci), len(loci))
     
     snp_matrix = {}
+    tokenize = {
+        b'A': 1,
+        b'T': 2,
+        b'C': 3,
+        b'G': 4,
+    }
+    
+    for position in loci:
+        # unsigned byte
+        snp_matrix[position] = np.zeros(1508, dtype=np.dtype('B'))
+    
+    print("finished snp matrix prep;")
+    view_matrix(snp_matrix)
     
     for count, file in enumerate(os.scandir("../dataset")):
         genome_id, variant_type = file.name.rstrip(".txt").split("_")
@@ -39,14 +96,28 @@ def add_snps() -> np.array:
         if variant_type != 'snps':
             continue
         
-        if count % 100 == 0:
-            print(f'dataframe at file count {count} = ', df.info(), df)
+        if count % 1 == 0:
+            print(f"\n\t[{count/2}/1508]")
+            view_matrix(snp_matrix)
         
         with open("../dataset/" + file.name, "r") as f:
-        
-            for line in f:
-                position, reference, alternate, quality = line.split()
+            
+            X = list(np.loadtxt(f, dtype=int, usecols=(0, 2),
+                converters={
+                    2: lambda x: tokenize[x]
+                }
+            ))
+
+            # hash for snp
+            for position, snp in X:
+                snp_matrix[position][count] = tokenize[snp]
                 
-       
-        # TODO need all snp locations in advance (2 passes)
+                
+    with open("../results/snp_matrix", "wb") as snp_matrix_file:
+        pickle.dump(snp_matrix, snp_matrix_file)
         
+        
+        
+        # TODO issues for when you return
+        # variant count problem
+        # line 107 gets 2 nucleotides at once to tokinze, makes no sense
